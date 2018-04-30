38865667bac8690c03100c89afe8290ac199b3b4
RuslanKoshel <38784656+RuslanKoshel@users.noreply.github.com>
2018-04-30 11:32:34 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,101 @@
+// document.getElementById('home').onclick = home;
+// function home() {
+// 	//var home = document.getElementById('home');
+// 	//var home = document.getElementsByTagName('a');
+// 	 //var tableElem = document.getElementById('home');
+//  	 var elements = document.getElementsByTagName('a')[0];
+//  	 //var elem = elements.getElementsByTagName('color');
+
+//  	  console.log(elements);
+ 	
+	
+// 	style = elements.style;
+// 	style.color = "red";
+// 	style.backgroundColor = "red";
+//     // var about = document.getElementById('about');
+//     // style = about.style;
+//     // style.color = "##212121";
+//     // var contact = document.getElementById('contact');
+//     // style = contact.style;
+//     // style.color = "##212121";
+// }
+// document.getElementById('about').onclick = about;
+// function about() {
+// 	var about = document.getElementById('about');
+// 	style = about.style;
+// 	style.color = "#10c9c3";
+//     var home = document.getElementById('home');
+//     style = home.style;
+//     style.color = "##212121";
+//     var contact = document.getElementById('contact');
+//     style = contact.style;
+//     style.color = "##212121";
+// }
+// document.getElementById('contact').onclick = contact;
+// function contact() {
+// 	var contact = document.getElementById('contact');
+// 	style = contact.style;
+// 	style.color = "#10c9c3";
+//     var home = document.getElementById('home');
+//     style = home.style;
+//     style.color = "##212121";
+//     var about = document.getElementById('about');
+//     style = about.style;
+//     style.color = "##212121";
+// }
+
+var modal =document.querySelector('.modal');
+
+document.getElementById('galleryImg').onclick = galleryImg;
+function galleryImg() {
+	document.querySelector('#galleryImg').addEventListener('click',function(e){
+	var im = e.target.id;
+	var polosa = document.getElementById('polosa');
+	polosa.style.top = im +'px';
+	modal.style.display = 'flex'
+})
+
+}
+
+document.getElementById('close').onclick = close;
+function close() {
+	var close = document.querySelector('.pe-7s-close');
+	modal.style.display = 'none';
+}
+
+window.addEventListener('click',function(e){
+	if (e.target == modal){
+		modal.style.display = 'none';
+	} 
+})
+
+
+document.getElementById('sliderLeft').onclick = sliderLeft;
+document.getElementById('sliderRight').onclick = sliderRight;
+var top1 = 0;
+var bottom = 0;
+
+function sliderLeft() {
+	
+	var polosa = document.getElementById('polosa');
+	top1 = top1+552.4545;
+	if (top1> 0) {
+		top1 = -6077;
+	}
+	polosa.style.top = top1 +'px';
+
+}
+function sliderRight() {
+	
+	var polosa = document.getElementById('polosa');
+	top1 = top1-552.4545;
+	if (top1< -6077) {
+		top1 = 0;
+	}
+	polosa.style.top = top1+'px';
+}
+
+// document.querySelector('#galleryImg').addEventListener('click',function(e){
+// 	var im = e.target.id;
+// 	alert(im);
+// })
